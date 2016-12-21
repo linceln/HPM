be35409e9516e8b43652ccb1adc8adc851d76e08
PC-20160726UFPH\Administrator <flycjh@126.com>
2016-12-21 17:58:12 +0800

    SQlite

--- app/src/main/java/com/olsplus/balancemall/core/db/MessageDBHelper.java
+++ app/src/main/java/com/olsplus/balancemall/core/db/MessageDBHelper.java
@@ -1,8 +1,30 @@
 package com.olsplus.balancemall.core.db;
 
-/**
- * Created by Administrator on 2016/12/21 0021.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class MessageDBHelper {
+
+public class MessageDBHelper extends SQLiteOpenHelper {
+
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "message.db";
+    public static final String TABLE_NAME = "messages";
+
+    public MessageDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table if not exists " + TABLE_NAME + " (serial integer primary key, id text)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+    }
 }
